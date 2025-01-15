bc33034fe634f9bb73d14f715840bda39d761843
Sevotyanov Aleksandr <sevostanovsasa7@gmail.com>
2025-01-15 17:12:38 +0300

    chore: update swagger doc URL

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/swagger', app, document);
 
   await app.listen(8080);
 }
